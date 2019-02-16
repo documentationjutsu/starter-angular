2611c658b87e830ae8a45dd23f92f5875edf23a3
Ilan H <ilan@pop-os.localdomain>
2019-02-15 23:10:39 -0600

    add example module and component

---
+++ src/app/example/example.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css']
+})
+export class ExampleComponent implements OnInit {
+
+  example: string = 'Example Component'
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
---
+++ src/app/example/example.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExampleComponent } from './example.component';
+
+@NgModule({
+  declarations: [ExampleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExampleModule { }
